7a04ee1e27a3b62a0e38dc30161ee4a240b351d6
Juraj Vajda <juraj.vajda@sakeldevinc.ca>
2021-03-20 14:25:49 -0400

    recipes update

--- items.lua
+++ items.lua
@@ -15,9 +15,9 @@ x_bows.register_arrow('arrow_wood', {
 	description = 'Arrow Wood',
 	inventory_image = 'x_bows_arrow_wood.png',
 	craft = {
-		{'', 'default:flint', ''},
-		{'', 'group:stick', ''},
-		{'', 'group:wool', ''}
+		{'default:flint'},
+		{'group:stick'},
+		{'group:wool'}
 	},
 	tool_capabilities = {
 		full_punch_interval = 1,
@@ -30,9 +30,9 @@ x_bows.register_arrow('arrow_stone', {
 	description = 'Arrow Stone',
 	inventory_image = 'x_bows_arrow_stone.png',
 	craft = {
-		{'', 'default:flint', ''},
-		{'', 'group:stone', ''},
-		{'', 'group:wool', ''}
+		{'default:flint'},
+		{'group:stone'},
+		{'group:wool'}
 	},
 	tool_capabilities = {
 		full_punch_interval = 1.2,
@@ -45,9 +45,9 @@ x_bows.register_arrow('arrow_bronze', {
 	description = 'Arrow Bronze',
 	inventory_image = 'x_bows_arrow_bronze.png',
 	craft = {
-		{'', 'default:flint', ''},
-		{'', 'default:bronze_ingot', ''},
-		{'', 'group:wool', ''}
+		{'default:flint'},
+		{'default:bronze_ingot'},
+		{'group:wool'}
 	},
 	tool_capabilities = {
 		full_punch_interval = 0.8,
@@ -60,9 +60,9 @@ x_bows.register_arrow('arrow_steel', {
 	description = 'Arrow Steel',
 	inventory_image = 'x_bows_arrow_steel.png',
 	craft = {
-		{'', 'default:flint', ''},
-		{'', 'default:steel_ingot', ''},
-		{'', 'group:wool', ''}
+		{'default:flint'},
+		{'default:steel_ingot'},
+		{'group:wool'}
 	},
 	tool_capabilities = {
 		full_punch_interval = 0.7,
@@ -75,9 +75,9 @@ x_bows.register_arrow('arrow_mese', {
 	description = 'Arrow Mese',
 	inventory_image = 'x_bows_arrow_mese.png',
 	craft = {
-		{'', 'default:flint', ''},
-		{'', 'default:mese_crystal', ''},
-		{'', 'group:wool', ''}
+		{'default:flint'},
+		{'default:mese_crystal'},
+		{'group:wool'}
 	},
 	tool_capabilities = {
 		full_punch_interval = 0.7,
@@ -90,9 +90,9 @@ x_bows.register_arrow('arrow_diamond', {
 	description = 'Arrow Diamond',
 	inventory_image = 'x_bows_arrow_diamond.png',
 	craft = {
-		{'', 'default:flint', ''},
-		{'', 'default:diamond', ''},
-		{'', 'group:wool', ''}
+		{'default:flint'},
+		{'default:diamond'},
+		{'group:wool'}
 	},
 	tool_capabilities = {
 		full_punch_interval = 0.7,
@@ -106,8 +106,8 @@ x_bows.register_arrow('arrow_diamond_tipped_poison', {
 	inventory_image = 'x_bows_arrow_diamond_poison.png',
 	craft = {
 		{'', '', ''},
-		{'', 'farming_addons:poisonouspotato', ''},
-		{'', 'x_bows:arrow_wood', ''}
+		{'', 'default:marram_grass_1', ''},
+		{'', 'x_bows:arrow_diamond', ''}
 	},
 	tool_capabilities = {
 		full_punch_interval = 0.7,
